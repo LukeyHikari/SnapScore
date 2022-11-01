f3154b1b20df40a7d7d32f4dab67677dcbab80aa
Lukey <lukerosetepons@gmail.com>
2022-11-01 14:23:14 +0800

    uwu

--- App/main.py
+++ App/main.py
@@ -1,3 +1,4 @@
+from sys import _xoptions
 import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 from kivy.app import App
@@ -5,7 +6,8 @@ from kivy.uix.label import Label
 
 class ISSRA(App):
     def build(self):
-        return Label(text='Hello world')
+        x = Label(text= 'Jericho')
+        return x
 
 
 if __name__ == '__main__':
