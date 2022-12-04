88515a62ae9b0d81abba7bffd546673189088713
Lukey <lukerosetepons@gmail.com>
2022-12-04 16:20:06 +0800

    Change to KivyMD

--- App/main.py
+++ App/main.py
@@ -1,14 +1,41 @@
 from sys import _xoptions
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.lang import Builder
 
-class ISSRA(App):
+KV = '''
+MDScreen:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+
+                MDTopAppBar:
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#90abe0"
+                    specific_text_color: "#4a4939"
+                    left_action_items:
+                        [['menu', lambda x: nav_drawer.set_state("open")]]
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            ContentNavigationDrawer:
+'''
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
+
+class ISSRA(MDApp):
     def build(self):
-        x = Label(text= 'Jericho')
-        y = Label(text= 'Jericho')
-        return y
+        self.theme_cls.theme_style = "Light"
+        screen = Builder.load_string(KV)
+        return screen
 
 if __name__ == '__main__':
     ISSRA().run()
