fa52cd5928ce469c1b42d47905afeebd0b1c18ce
Lukey <lukerosetepons@gmail.com>
2022-12-28 17:22:54 +0800

    UI Improvement
    - Added logos
    - Replaced NavDrawer's TopAppBar to be called by a button

--- App/main.py
+++ App/main.py
@@ -1,8 +1,11 @@
 from sys import _xoptions
 from kivymd.app import MDApp
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, FadeTransition  
 from kivy.lang import Builder
 from kivy.clock import Clock
+
+Window.size = (480,800)
 
 class SnapScore(MDApp):
     global smanager
@@ -15,12 +18,12 @@ class SnapScore(MDApp):
         smanager.add_widget(Builder.load_file('snapscore.kv'))   
         return smanager
 
-    def on_start(self):
-        Clock.schedule_once(self.change_screen, 3)
+    def on_start(self): #Function that will be called on start
+        Clock.schedule_once(self.change_screen, 5)
 
-    def change_screen(self, dt):
+    def change_screen(self, dt): #Open->Main Screen Transition function
         smanager.transition = FadeTransition(clearcolor=(0,0,0,0))
-        smanager.duration = 2 
+        smanager.duration = 3 
         smanager.current = "mscreen"
 
 if __name__ == '__main__':
